ef2439d6a6d80b94a3dc695402c8e33cac61fbc8
Davi Raeder <98568443+DaviRaeder@users.noreply.github.com>
2022-02-19 18:20:23 -0300

    criei o ex008

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 12.9
+console.log('A velocidade do seu carro é ' + vel + ' km/h')
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+    var seguranca = console.log('Dirija sempre usando cinto de segurança!')
+
+
