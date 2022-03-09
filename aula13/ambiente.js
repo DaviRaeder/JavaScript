c5d8fe14db715a7b2f726e53a09206144f0417c1
Davi Raeder <98568443+DaviRaeder@users.noreply.github.com>
2022-03-09 20:46:29 -0300

    atualizei o ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+ var c = 1
+ do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+ } while (c <= 6)
+
+
+
+
+ /* while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+ }
+*/
+
+
+
+
+ /*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
