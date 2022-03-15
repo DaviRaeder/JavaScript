75420524274cb26a259d6ecdddaf0ec117321b36
Davi Raeder <98568443+DaviRaeder@users.noreply.github.com>
2022-03-15 16:56:17 -0300

    atualizei o ambiente.js da aula 15

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,9 +1,13 @@
-let num = [5, 8, 2, 9, 3]
+let num = [5, 8, 2, 9, 3] // 2 3 5 8 9 
 num.push(1)
 num.sort()
-
-console.log(`Nosso vetor é o ${num}`)
-
+console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é o ${num[0]}`)
+let pos = num.indexOf(4)
+if (pos == -1) {
+    console.log('O valor não foi encontrado')   
+} else {
+    console.log(`O valor 4 está na posição ${pos}`)
+}
 
-console.log
