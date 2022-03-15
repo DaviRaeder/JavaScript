e5b2175eab002cf19c91ad3957b1a960585ec2c6
Davi Raeder <98568443+DaviRaeder@users.noreply.github.com>
2022-03-15 17:03:04 -0300

    atualizei a funcao01.js da aula16

--- aula16/funcao01.js
+++ aula16/funcao01.js
@@ -2,6 +2,9 @@ function parimpar (n) {
     if (n%2 == 0) {
         return 'Par!'
     } else {
-        
+        return 'Impar!'
     }
 }
+
+let res = parimpar(895)
+console.log(`O número é ${res}`)
