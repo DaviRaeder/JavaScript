c76e092eb55f690a7986ba638ea3b5111fa8b764
Davi Raeder <98568443+DaviRaeder@users.noreply.github.com>
2022-05-19 14:59:47 -0300

    atualizei o JavaScript

--- aula21/functionpt2.js
+++ aula21/functionpt2.js
@@ -28,8 +28,44 @@ var PersonagemThis = {
     nome: 'Teshima'
 }
 
-Personagem.call(PersonagemThis, 'param1', 123, [5])
+// Personagem.call(PersonagemThis, 'param1', 123, [5])
 
 // apply
 
-Personagem.apply(PersonagemThis, ['param$$$$$', 123, [5]])
+// var personagemParams = (PersonagemThis, ['param$$$$$', 321, [5]])
+// Personagem.apply(PersonagemThis, personagemParams)
+
+// bind
+
+var pers = Personagem.bind(PersonagemThis,'Olá')
+pers('Mundo', '!!!')
+
+// Arrow functions
+
+/* var soma = (a, b) => {
+    return a + b
+} */
+
+var soma = (a, b) => ( 
+    a + b 
+)
+
+console.log(soma(10, 11));
+
+var retornaUsuario = cidade => (
+    { 
+       nome: 'Ayrton',
+       idade: 27,
+       cidade
+    }
+  )
+
+console.log(retornaUsuario('Rio de Janeiro 123'));
+
+ var imprimiThis = () => {
+     console.log(this)
+ }
+
+ imprimiThis()
+
+ imprimiThis.call({nome: 'Ayrton'})
